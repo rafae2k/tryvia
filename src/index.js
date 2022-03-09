5f4c09fcd6834f9fe6ffd368fb4f4cf3e165bb37
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-03-09 17:32:30 -0300

    feat(redux): create redux

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import App from './App';
+import store from './redux/store';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <StrictMode>
+    <Provider store={ store }>
+      <App />
+    </Provider>
+  </StrictMode>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/redux/actions/rankingActions.js
@@ -0,0 +1,6 @@
+export const SET_USER_RANKING = 'SET_USER_RANKING';
+
+export const setRankingAction = (state) => ({
+  type: SET_USER_RANKING,
+  payload: state,
+});
