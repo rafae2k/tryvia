5088a06b5e5b857b941f796d042046643ab67479
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-03-10 11:01:09 -0300

    feat(feedback): create Feedback page

---
+++ src/pages/Feedback/index.js
@@ -0,0 +1,15 @@
+// import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+import { Header } from '../../components/Header';
+
+export class Feedback extends Component {
+  render() {
+    return (
+      <Header />
+    );
+  }
+}
+
+Feedback.propTypes = {};
+
+export default Feedback;
