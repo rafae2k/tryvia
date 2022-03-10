b2079a7f7190b35f11003d30261c9290ea23553a
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-03-10 19:25:25 -0300

    feat(gravatar): implement gravatar fetch

--- src/redux/actions/playerActions.js
+++ src/redux/actions/playerActions.js
@@ -1,3 +1,5 @@
+import md5 from 'crypto-js/md5';
+
 export const ADD_PLAYER = 'ADD_PLAYER';
 export const SET_SCORE = 'SET_SCORE';
 
@@ -10,3 +12,17 @@ export const setScore = (state) => ({
   type: SET_SCORE,
   payload: state,
 });
+
+export function fetchGravatar(gravatarEmail) {
+  const HASH = md5(gravatarEmail).toString();
+  const URL = `https://www.gravatar.com/avatar/${HASH}`;
+
+  return async (dispatch) => {
+    try {
+      const response = await fetch(URL);
+      const data = await response.json();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+}
