fffe94909ee2bd9780ca2364f745536b89ee8d29
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-03-11 16:48:52 -0300

    refactor(localStorage): naming

--- src/services/localStorage.service.js
+++ src/services/localStorage.service.js
@@ -1,8 +1,8 @@
 const TOKEN = 'token';
 
 // Read token from local localStorage
-export const readToken = () => JSON.parse(localStorage.getItem(TOKEN));
+export const readTokenFromLocalStorage = () => JSON.parse(localStorage.getItem(TOKEN));
 
 // Save token on local localStorage
-export const saveToken = (triviaToken) => localStorage
+export const saveTokenOnLocalStorage = async (triviaToken) => localStorage
   .setItem(TOKEN, JSON.stringify(triviaToken));
