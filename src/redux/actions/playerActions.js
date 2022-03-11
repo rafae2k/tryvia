572f7adbf044416f18dcd39c078e14eadd3d54d6
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-03-10 21:02:59 -0300

    remove(fetchAvatar): remove fetch avatar thunk

--- src/redux/actions/playerActions.js
+++ src/redux/actions/playerActions.js
@@ -1,5 +1,3 @@
-import md5 from 'crypto-js/md5';
-
 export const ADD_PLAYER = 'ADD_PLAYER';
 export const SET_SCORE = 'SET_SCORE';
 
@@ -12,17 +10,3 @@ export const setScore = (state) => ({
   type: SET_SCORE,
   payload: state,
 });
-
-export function fetchGravatar(gravatarEmail) {
-  const HASH = md5(gravatarEmail).toString();
-  const URL = `https://www.gravatar.com/avatar/${HASH}`;
-
-  return async (dispatch) => {
-    try {
-      const response = await fetch(URL);
-      const data = await response.json();
-    } catch (error) {
-      console.error(error);
-    }
-  };
-}
