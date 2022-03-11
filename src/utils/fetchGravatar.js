df3f4a5c43883276a88504fcd7e4c49319e310b0
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-03-11 16:49:35 -0300

    refactor: move logic to game page

--- src/utils/fetchGravatar.js
+++
@@ -1,9 +0,0 @@
-import md5 from 'crypto-js/md5';
-
-const getGravatar = (gravatarEmail) => {
-  const HASH = md5(gravatarEmail).toString();
-  const URL = `https://www.gravatar.com/avatar/${HASH}`;
-  return URL;
-};
-
-export default getGravatar;
