24e2b32b1210d3a3a6449640d7dd7f63287a66aa
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-03-11 16:47:37 -0300

    feat(triviaAPI): implement expired token logic

--- src/redux/actions/triviaApi.js
+++ src/redux/actions/triviaApi.js
@@ -1,4 +1,9 @@
-import { readToken } from '../../services/localStorage.service';
+import {
+  readTokenFromLocalStorage,
+  saveTokenOnLocalStorage,
+} from '../../services/localStorage.service';
+import fetchTriviaToken from '../../utils/fetchTriviaToken';
+import { setTriviaToken } from './tokenActions';
 
 // Fetch Questions actions
 export const GET_TRIVIA_QUESTIONS = 'GET_TRIVIA_QUESTIONS';
@@ -9,16 +14,24 @@ export const getTriviaQuestions = (state) => ({
 });
 
 // Thunk
-export function fetchQuestions() {
-  const token = readToken();
-  const URL = `https://opentdb.com/api.php?amount=5&token=${token}`;
-  return async (dispatch) => {
-    try {
-      const response = await fetch(URL);
-      const data = await response.json();
+export const fetchQuestions = () => async (dispatch) => {
+  try {
+    let token = readTokenFromLocalStorage();
+
+    let response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+    let data = await response.json();
+
+    if (data.response_code === 0) {
       dispatch(getTriviaQuestions(data.results));
-    } catch (error) {
-      console.error(error);
+    } else if (data.response_code !== 0) {
+      token = await fetchTriviaToken();
+      await saveTokenOnLocalStorage(token);
+      dispatch(setTriviaToken(token));
+      response = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+      data = await response.json();
+      dispatch(getTriviaQuestions(data.results));
     }
-  };
-}
+  } catch (error) {
+    console.error(error);
+  }
+};
