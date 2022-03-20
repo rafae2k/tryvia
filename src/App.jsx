6a8d5037ebaab2caf8c362e8839809e4c19da933
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-03-20 14:33:47 -0300

    styles(app): add input and logo components

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+// Components
 import Login from './pages/Login';
 import Game from './pages/Game';
 import Configurations from './pages/Configurations';
 import Feedback from './pages/Feedback';
 import Ranking from './pages/Ranking';
 
-import './App.css';
-// import logo from './trivia.png';
-
 export default function App() {
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-      </header> */}
       <Switch>
         <Route exact path="/" component={ Login } />
         <Route path="/Game" component={ Game } />
