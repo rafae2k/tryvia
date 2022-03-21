31eca1a3415a21435bb52d7b720ba271b62e2442
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-03-20 21:40:39 -0300

    styles(timer): add countdown

--- src/components/Timer/index.jsx
+++ src/components/Timer/index.jsx
@@ -1,102 +1,10 @@
-// import React, { Component } from 'react';
-// // import PropTypes from 'prop-types';
-
-// class Timer extends Component {
-//   state = {
-//     currentTime: 30,
-//   }
-
-//   componentDidMount() {
-//     const ONE_SECOND_IN_MILLISECONDS = 1000;
-//     this.timerID = setInterval(
-//       () => this.tick(),
-//       ONE_SECOND_IN_MILLISECONDS,
-//     );
-//   }
-
-//   tick() {
-//     this.setState((prevState) => ({
-//       currentTime: prevState.currentTime - 1,
-//     }));
-//   }
-
-//   render() {
-//     const { currentTime } = this.state;
-//     return (
-//       <div>
-//         { (currentTime > 0)
-//           ? <h1>{ currentTime }</h1>
-//           : <h1>0</h1>
-//         }
-//       </div>
-//     );
-//   }
-// }
-
-// // Timer.propTypes = {
-// //   currentTime: PropTypes.number,
-// // }.isRequired;
-
-// export default Timer;
-
-// ---------------------------------------
-
-// import React from 'react';
-
-// class Timer extends React.Component {
-//   constructor() {
-//     super();
-
-//     this.state = {
-//       counter: 30,
-//       // timeOut: false,
-//     };
-//     this.timer = null;
-//   }
-
-//   componentDidMount() {
-//     const ONE_SECOND = 1000;
-//     this.timer = setInterval(
-//       () => this.setState((prevState) => ({ counter: prevState.counter - 1 })),
-//       ONE_SECOND,
-//     );
-//   }
-
-//   // recebo PropsAntigas , EstadoAntigo
-//   componentDidUpdate(prevProps, prevState) {
-//     const TIME_LIMIT = 0;
-
-//     const { counter/* , timeOut */ } = prevState;
-
-//     if (counter === TIME_LIMIT) {
-//       this.setState({
-//         counter: 30,
-//         // timeOut: true,
-//       });
-//     }
-//     console.log('Atualizando o estado do componente');
-//   }
-
-//   componentWillUnmount() {
-//     console.log('Desmonatando o componente');
-//     clearInterval(this.timer);
-//   }
-
-//   render() {
-//     const { counter } = this.state;
-
-//     return (
-//       <section>
-//         <h2>{counter}</h2>
-//       </section>
-//     );
-//   }
-// }
-
-// export default Timer;
-
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
+
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
+
+import { Container } from './styles';
+
 
 function Timer({
   // getTimeLeft,
@@ -136,11 +44,16 @@ function Timer({
   }, [timer, isTimerEnabled, resetTimer]);
 
   return (
-    <section>
-      <br />
-      Tempo:
-      <h2>{timer < 0 ? '0' : timer}</h2>
-    </section>
+    <Container>
+      <CountdownCircleTimer
+        isPlaying={ isTimerEnabled }
+        duration={ 30 }
+        colors={ ['#7B61FF', '#E12C2C', '#FFA500', '#FFD700', '#FFDF00'] }
+        colorsTime={ [30, 7, 8, 10] }
+      >
+        {({ remainingTime }) => remainingTime}
+      </CountdownCircleTimer>
+    </Container>
   );
 }
 
---
+++ src/components/Timer/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
+export const none = styled.div``;
