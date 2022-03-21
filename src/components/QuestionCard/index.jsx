0a2380e40a3e3170795de5d4f803a40af00a39f2
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-03-20 21:43:45 -0300

    styles(questionCard): QuestionCard styling

--- src/components/QuestionCard/index.jsx
+++ src/components/QuestionCard/index.jsx
@@ -2,8 +2,14 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { v4 as uuid } from 'uuid';
 
+// Services/utils
+import decodeHtml from '../../utils/decodeHtml';
+
+// Components
+import Timer from '../Timer';
+
 // Styles
-import { QuestionContainer, AnswersContainer, Button } from './styles';
+import { Container, QuestionContainer, AnswersContainer, Button } from './styles';
 
 const QuestionCard = ({
   question: triviaQuestion,
@@ -11,6 +17,11 @@ const QuestionCard = ({
   isAnswersDisabled,
   userClicked,
   startTimer,
+  getTimeLeft,
+  isTimerEnabled,
+  setTimeOver,
+  resetTimer,
+  setResetTimer,
 }) => {
   useEffect(() => {
     startTimer();
@@ -48,20 +59,32 @@ const QuestionCard = ({
 
   // Return the answers buttons based on randomizeAnswers array
   return (
-    <QuestionContainer>
-      <h2
-        data-testid="question-category"
-        dangerouslySetInnerHTML={ { __html: category } }
-      />
+    <Container>
+      <QuestionContainer>
+        <div className="question-border">
+          <div className="question-content">
+            <h1
+              data-testid="question-category"
+              dangerouslySetInnerHTML={ { __html: category } }
+            />
+            <h2
+              data-testid="question-text"
+              // dangerouslySetInnerHTML solves the problem of strange html characters
+              // but in this specific one element whe face some tests problems
+              dangerouslySetInnerHTML={ { __html: question } }
+            />
 
-      <h3
-        data-testid="question-text"
-        // dangerouslySetInnerHTML solves the problem of strange html characters
-        // but in this specific one element whe face some tests problems
-        // dangerouslySetInnerHTML={ { __html: question } }
-      >
-        {question}
-      </h3>
+            <Timer
+              getTimeLeft={ getTimeLeft }
+              isTimerEnabled={ isTimerEnabled }
+              setTimeOver={ setTimeOver }
+              resetTimer={ resetTimer }
+              setResetTimer={ setResetTimer }
+            />
+          </div>
+        </div>
+
+      </QuestionContainer>
       <AnswersContainer data-testid="answer-options">
         {randomizeAnswers.map((quiz, index) => (
           (correctAnswer === quiz)
@@ -91,7 +114,7 @@ const QuestionCard = ({
             )
         ))}
       </AnswersContainer>
-    </QuestionContainer>
+    </Container>
   );
 };
 
--- src/components/QuestionCard/styles.js
+++ src/components/QuestionCard/styles.js
@@ -1,6 +1,51 @@
 import styled from 'styled-components';
 
+export const Container = styled.div`
+  height: 100%;
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 9rem;
+`;
+
 export const QuestionContainer = styled.div`
+  max-width: 25rem;
+
+  text-align: center;
+
+  filter: drop-shadow(-3px 6px 0 #452D71)
+          drop-shadow(-5px 6px 0 #452D71);
+  transition: .4s ease-in-out;
+
+  .question-border {
+    width: 105%;
+    height: 105%;
+
+    display: grid;
+    place-content: center;
+
+    background-color: #452D71;
+    clip-path: polygon(0% 5%, 10% 5%, 10% 0%, 90% 0%, 90% 5%, 100% 5%, 100% 95%, 90% 95%, 90% 100%, 10% 100%, 10% 95%, 0% 95%);
+    transform: skewX(5deg);
+  }
+
+  .question-content {
+    min-height: 27rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1.5rem;
+
+    padding: 3rem 4.2rem 5rem;
+
+    background-color: white;
+    clip-path: polygon(0% 5%, 10% 5%, 10% 0%, 90% 0%, 90% 5%, 100% 5%, 100% 95%, 90% 95%, 90% 100%, 10% 100%, 10% 95%, 0% 95%);
+
+    /* transform: skewX(5deg); */
+  }
 `;
 
 export const AnswersContainer = styled.div`
