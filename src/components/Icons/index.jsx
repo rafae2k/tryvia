f740129df31af208ae39d7003877986c69bb80af
Rafael Feitoza <53544360+rafae2k@users.noreply.github.com>
2022-03-20 21:43:08 -0300

    chore: add toggle dark mode icons

---
+++ src/components/Icons/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export const uncheckedIcon = (
+
+  <svg width="24" height="24" viewBox="0 0 34 34" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M33.5 18.4483C33.2116 21.569 32.0404 24.5431 30.1235 27.0224C28.2065 29.5018 25.6231 31.384 22.6755 32.4487C19.7278 33.5134 16.538 33.7166 13.4791 33.0345C10.4202 32.3524 7.61882 30.8133 5.40275 28.5973C3.18667 26.3812 1.64756 23.5798 0.965499 20.5209C0.283441 17.462 0.486648 14.2722 1.55135 11.3245C2.61604 8.37693 4.49819 5.7935 6.97756 3.87655C9.45693 1.95959 12.431 0.788393 15.5517 0.5C13.7246 2.97182 12.8454 6.01732 13.074 9.08259C13.3026 12.1479 14.6237 15.0293 16.7972 17.2028C18.9707 19.3763 21.8521 20.6974 24.9174 20.926C27.9827 21.1546 31.0282 20.2754 33.5 18.4483Z" fill="#306FF6" />
+  </svg>
+
+);
+
+export const checkedIcon = (
+
+  <svg width="24" height="24" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M15 21.25C18.4518 21.25 21.25 18.4518 21.25 15C21.25 11.5482 18.4518 8.75 15 8.75C11.5482 8.75 8.75 11.5482 8.75 15C8.75 18.4518 11.5482 21.25 15 21.25Z" fill="#FCEC4F" />
+    <path d="M15 1.25V3.75" stroke="#FCEC4F" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M15 26.25V28.75" stroke="#FCEC4F" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M5.27499 5.2749L7.04999 7.0499" stroke="#FCEC4F" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M22.95 22.9502L24.725 24.7252" stroke="#FCEC4F" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M1.25 15H3.75" stroke="#FCEC4F" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M26.25 15H28.75" stroke="#FCEC4F" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M5.27499 24.7252L7.04999 22.9502" stroke="#FCEC4F" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M22.95 7.0499L24.725 5.2749" stroke="#FCEC4F" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+);
